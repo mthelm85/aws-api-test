bda9441c565bb61d7048c1bf96fc4129c9a29090
Matt Helm <mthelm85@gmail.com>
2024-10-22 08:38:50 -0700

    change port to 8080

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ app.get('/health', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
